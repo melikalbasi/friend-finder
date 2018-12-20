959a538ea10a6830412fc38c735069345fa0960c
Melika Kalbasi <melikalbasi@gmail.com>
2018-12-19 20:27:33 -0800

    add survey questions, changes to html files, and add friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,215 +1,228 @@
 var friends = [
   {
-    "name":"Michael",
-    "photo":"http://2.bp.blogspot.com/-wdvOTaqFTgc/VRuiScblddI/AAAAAAAADsk/a3ZaYhlu35o/s1600/AIDSjoke.jpg",
-    "scores":[
+    "name": "Michael",
+    "photo": "http://2.bp.blogspot.com/-wdvOTaqFTgc/VRuiScblddI/AAAAAAAADsk/a3ZaYhlu35o/s1600/AIDSjoke.jpg",
+    "scores": [
       5,
       5,
+      1,
+      3,
+      2,
+      1,
       5,
       5,
+      1,
+      1
+    ]
+  },
+  {
+    "name": "Dwight",
+    "photo": "https://i.imgflip.com/1cx1mk.jpg",
+    "scores": [
+      1,
+      2,
+      5,
+      1,
+      3,
+      1,
+      1,
+      3,
+      1,
+      1
+    ]
+  },
+  {
+    "name": "Kevin",
+    "photo": "https://gunaxin.com/wp-content/uploads/2010/08/Kevin_malone.jpg",
+    "scores": [
+      5,
+      4,
+      1,
+      5,
+      1,
+      2,
+      1,
+      5,
+      1,
+      1
+    ]
+  },
+  {
+    "name": "Stanley",
+    "photo": "https://alchetron.com/cdn/stanley-hudson-d60967a9-db6e-4bd8-bfd5-be4e49d9015-resize-750.jpeg",
+    "scores": [
+      "2",
+      "1",
+      "1",
+      "5",
+      "1",
+      "1",
+      "1",
+      "4",
+      "1",
+      "1"
+    ]
+  },
+  {
+    "name": "Creed",
+    "photo": "https://i.imgur.com/XgSAbF1.png",
+    "scores": [
+      "5",
+      "2",
+      "2",
+      "2",
+      "2",
+      "1",
+      "1",
+      "2",
+      "5",
+      "1"
+    ]
+  },
+  {
+    "name": "Ryan",
+    "photo": "https://i.imgur.com/TOZALH2.jpg",
+    "scores": [
+      "5",
+      "1",
+      "1",
+      "1",
+      "5",
+      "1",
+      "5",
+      "2",
+      "1",
+      "1"
+    ]
+  },
+  {
+    "name": "Andy",
+    "photo": "https://i.pinimg.com/originals/86/be/d5/86bed5916463c0b555c598f625a469ef.jpg",
+    "scores": [
+      "4",
+      "4",
+      "1",
+      "1",
+      "4",
+      "2",
+      "5",
+      "5",
+      "1",
+      "5"
+    ]
+  },
+  {
+    "name": "Kelly",
+    "photo": "http://38.media.tumblr.com/ce2ca73b1d44a4f0a9622e63a8ebcdc3/tumblr_nhgydadsVP1rbgu1so1_500.gif",
+    "scores": [
+      5,
+      1,
+      1,
+      3,
+      2,
+      1,
       5,
       5,
-      5,
-      5,
-      5,
-      5
-      ]
+      1,
+      1
+    ]
   },
   {
-    "name":"Dwight",
-    "photo":"https://i.imgflip.com/1cx1mk.jpg",
-    "scores":[
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-      ]
+    "name": "Erin",
+    "photo": "https://i.gifer.com/32xa.gif",
+    "scores": [
+      "5",
+      "5",
+      "2",
+      "3",
+      "1",
+      "1",
+      "1",
+      "5",
+      "1",
+      "3"
+    ]
   },
   {
-    "name":"Kelly",
-    "photo":"../images/jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "name": "Angela",
+    "photo": "https://66.media.tumblr.com/b8eeb939ab6a1f0fbdd3bb12bbafdeee/tumblr_npqybhfJlI1r7pekso3_500.gif",
+    "scores": [
+      "1",
+      "1",
+      "2",
+      "1",
+      "2",
+      "5",
+      "5",
+      "3",
+      "1",
+      "3"
+    ]
   },
   {
-  "name": "Kevin",
-  "photo": "https://gunaxin.com/wp-content/uploads/2010/08/Kevin_malone.jpg",
-  "scores": [
-  "5",
-  "1",
-  "4",
-  "4",
-  "5",
-  "1",
-  "2",
-  "5",
-  "4",
-  "1"
-  ]
+    "name": "Pam",
+    "photo": "http://4.bp.blogspot.com/-W8iUqsUwaY0/VQW6ywZ2LcI/AAAAAAAACWA/E4K42DH_8-I/s1600/pam%2Bbeesly%2Brandom%2Bquote%2Bgenerator.jpg",
+    "scores": [
+      "1",
+      "2",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "5",
+      "1",
+      "1"
+    ]
   },
   {
-  "name": "Stanley",
-  "photo": "https://alchetron.com/cdn/stanley-hudson-d60967a9-db6e-4bd8-bfd5-be4e49d9015-resize-750.jpeg",
-  "scores": [
-  "4",
-  "2",
-  "5",
-  "1",
-  "3",
-  "2",
-  "2",
-  "1",
-  "3",
-  "2"
-  ]
+    "name": "Jim",
+    "photo": "https://memegenerator.net/img/instances/64536819.jpg",
+    "scores": [
+      "3",
+      "2",
+      "1",
+      "2",
+      "1",
+      "1",
+      "2",
+      "5",
+      "1",
+      "1"
+    ]
   },
   {
-  "name": "Creed",
-  "photo": "https://i.imgur.com/XgSAbF1.png",
-  "scores": [
-  "5",
-  "2",
-  "2",
-  "2",
-  "4",
-  "1",
-  "3",
-  "2",
-  "5",
-  "5"
-  ]
+    "name": "Oscar",
+    "photo": "https://66.media.tumblr.com/d4ec4a2164ed76448ca11e95b36be3f4/tumblr_ojxyqc78Is1u81ir6o1_500.gif",
+    "scores": [
+      "1",
+      "2",
+      "1",
+      "3",
+      "4",
+      "1",
+      "3",
+      "5",
+      "1",
+      "3"
+    ]
   },
   {
-  "name": "Ryan",
-  "photo": "https://i.imgur.com/TOZALH2.jpg",
-  "scores": [
-  "3",
-  "3",
-  "4",
-  "2",
-  "2",
-  "1",
-  "3",
-  "2",
-  "2",
-  "3"
-  ]
-  },
-  {
-  "name": "Andy",
-  "photo": "https://i.pinimg.com/originals/86/be/d5/86bed5916463c0b555c598f625a469ef.jpg",
-  "scores": [
-  "4",
-  "3",
-  "4",
-  "1",
-  "5",
-  "2",
-  "5",
-  "3",
-  "1",
-  "4"
-  ]
-  },
-  {
-  "name": "Kelly",
-  "photo": "http://38.media.tumblr.com/ce2ca73b1d44a4f0a9622e63a8ebcdc3/tumblr_nhgydadsVP1rbgu1so1_500.gif",
-  "scores": [
-  "4",
-  "4",
-  "2",
-  "3",
-  "2",
-  "2",
-  "3",
-  "2",
-  "4",
-  "5"
-  ]
-  },
-  {
-  "name": "Erin",
-  "photo": "https://i.gifer.com/32xa.gif",
-  "scores": [
-  "1",
-  "1",
-  "1",
-  "1",
-  "1",
-  "1",
-  "1",
-  "1",
-  "2",
-  "2"
-  ]
-  },
-  {
-  "name": "Angela",
-  "photo": "https://66.media.tumblr.com/b8eeb939ab6a1f0fbdd3bb12bbafdeee/tumblr_npqybhfJlI1r7pekso3_500.gif",
-  "scores": [
-  "5",
-  "5",
-  "4",
-  "2",
-  "5",
-  "2",
-  "4",
-  "1",
-  "5",
-  "3"
-  ]
-  },
-  {
-  "name": "Pam",
-  "photo": "http://4.bp.blogspot.com/-W8iUqsUwaY0/VQW6ywZ2LcI/AAAAAAAACWA/E4K42DH_8-I/s1600/pam%2Bbeesly%2Brandom%2Bquote%2Bgenerator.jpg",
-  "scores": [
-  "1",
-  "1",
-  "1",
-  "1",
-  "1",
-  "5",
-  "1",
-  "1",
-  "1",
-  "3"
-  ]
-  },
-  {
-  "name": "Jim",
-  "photo": "https://memegenerator.net/img/instances/64536819.jpg",
-  "scores": [
-  "3",
-  "3",
-  "3",
-  "3",
-  "3",
-  "3",
-  "2",
-  "3",
-  "3",
-  "3"
-  ]
-  },
-  ]
-
-
-
+    "name": "Phyllis",
+    "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2016-10/10/17/asset/buzzfeed-prod-web03/anigif_sub-buzz-23262-1476134937-2.gif",
+    "scores": [
+      "2",
+      "4",
+      "1",
+      "3",
+      "5",
+      "1",
+      "4",
+      "5",
+      "1",
+      "3"
+    ]
+  }
+]
 
 module.exports = friends;
